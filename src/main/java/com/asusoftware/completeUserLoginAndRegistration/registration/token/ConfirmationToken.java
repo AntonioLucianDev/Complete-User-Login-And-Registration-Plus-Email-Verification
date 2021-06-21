5553331d1511d16cdf3e99f132a48d6c52cb301c
Antonio Popa <web.antonio.popa@gmail.com>
2021-06-21 17:14:18 +0200

    Initial Commit

---
+++ src/main/java/com/asusoftware/completeUserLoginAndRegistration/registration/token/ConfirmationToken.java
@@ -0,0 +1,48 @@
+package com.asusoftware.completeUserLoginAndRegistration.registration.token;
+
+import com.asusoftware.completeUserLoginAndRegistration.appuser.AppUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+
+    @Id
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+    private UUID id;
+    @Column(nullable = false)
+    private String token;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+    private LocalDateTime confirmedAt;
+
+    // One user can have many confirmation tokens
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "app_user_id")
+    private AppUser appUser;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.appUser = appUser;
+    }
+}
